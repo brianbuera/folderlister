b1519e136b7e3df578f6dbf039c4c95265ba73be
Brian Buera <brianbuera2015@gmail.com>
2025-10-23 15:19:09 -0300

    Segundo commit: limpieza del codigo, todavia no tiene las funcionalidades, seguire haciendo test

--- enumerador.py
+++ enumerador.py
@@ -6,17 +6,9 @@ from datetime import datetime
 
 
 def enumerar(directorio: Path):
-    eliminados = []
-
-    if not any(directorio.iterdir()):
-        show_error(VACIO)
-        return
- 
-    if not validar_ruta(directorio):
-        show_error(NO_CUMPLE_REQUISTOS)
-        return
     
-    rutas_videos = obtenerRutasVideos(directorio)
+    eliminados = []
+    rutas_videos = obtenerVideosOrdenados(directorio)
 
     for idx, dir in enumerate(rutas_videos):
         carpeta= dir.parent
@@ -24,16 +16,23 @@ def enumerar(directorio: Path):
         nuevo_nombre = directorio / f"{str(idx+1).zfill(2)} - {carpeta.name}"
         
         if not nuevo_nombre.exists():
-            print(f"Movido a: {nuevo_nombre}\{obtenerHorario(dir.name).strftime("%H:%M %Y-%m-%d")}")
+            detalles(idx, dir, nuevo_nombre)
+            #carpeta.rename(nuevo_nombre)
         if eliminar.is_dir() and eliminar.parent == directorio:
             eliminados.append(eliminar)
-    print("\nLista de directorios a eliminar:") 
-    for e in eliminados:
-        print(e)
-    show_info("Exito","Los directorios se han enumerado correctamente")  
 
-        
-def obtenerRutasVideos(directorio: Path):
+    return True  
+
+def detalles(idx : int,dir : Path, nuevo_nombre : Path):
+    fecha_hora = obtenerHorario(dir.name).strftime("%H:%M %Y-%m-%d")
+    print(f"Directorio numero {idx+1}")
+    print(f"Ruta inicial: {str(dir)}")
+    print(f"Nuevo nombre: {str(nuevo_nombre)} - Hora y fecha: {fecha_hora}")
+    print(f"Nueva ruta del directorio: {str(nuevo_nombre)}")
+    print("")
+
+
+def obtenerVideosOrdenados(directorio: Path):
     rutas = []
     for dir in directorio.rglob("*.mkv"):
         rutas.append(dir)
--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from tkinter import filedialog
 from pathlib import Path
 from enumerador import enumerar
 from mensajes import *
+from validadores import validar_ruta
+
+
 
 if __name__ == "__main__":
-    directorio = filedialog.askdirectory()
-    if directorio:
-        enumerar(Path(directorio))
-    else:
-       show_error(NO_SELECCIONADO)
+
+    directorio : Path = Path(filedialog.askdirectory())
+
+    if not directorio:
+        show_error(NO_SELECCIONADO)
+        exit()       
+
+    if not any(directorio.iterdir()):
+        show_error(VACIO)
+        exit()
+    
+    if not validar_ruta(directorio):
+        show_error(NO_CUMPLE_REQUISTOS)
+        exit()
+    
+    enumerar(directorio)
+
